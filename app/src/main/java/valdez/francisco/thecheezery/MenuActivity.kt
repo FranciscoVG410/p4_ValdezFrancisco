e24ffe25f0188809b1f9b001f06c7ff2a7b15b6a
FranciscoVG410 <159390610+FranciscoVG410@users.noreply.github.com>
2025-09-13 23:11:48 -0700

    Practica 4 Completa

---
+++ app/src/main/java/valdez/francisco/thecheezery/MenuActivity.kt
@@ -0,0 +1,18 @@
+package valdez.francisco.thecheezery
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+    }
+}
