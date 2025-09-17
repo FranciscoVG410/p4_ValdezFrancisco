23813170ff0f9540b5dd7d4f2d43c57507a07ed5
FranciscoVG410 <159390610+FranciscoVG410@users.noreply.github.com>
2025-09-16 23:01:50 -0700

    the List of products was added

--- app/src/main/java/valdez/francisco/thecheezery/MenuActivity.kt
+++ app/src/main/java/valdez/francisco/thecheezery/MenuActivity.kt
@@ -7,12 +7,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import valdez.francisco.thecheezery.R.id.btnColdDrinks
+import valdez.francisco.thecheezery.R.id.btnHotDrinks
+import valdez.francisco.thecheezery.R.id.btnSweets
+import valdez.francisco.thecheezery.R.id.btnSalties
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        var btnColdDrinks: Button = findViewById(btnColdDrinks)
+        var btnHotDrinks: Button = findViewById(btnHotDrinks)
+        var btnSweets: Button = findViewById(btnSweets)
+        var btnSalties: Button = findViewById(btnSalties)
+
+        btnColdDrinks.setOnClickListener{
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("category", "cold")
+            startActivity(intent)
+        }
+
+        btnHotDrinks.setOnClickListener{
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("category", "hot")
+            startActivity(intent)
+        }
+
+        btnSweets.setOnClickListener{
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("category", "sweets")
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener{
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("category", "salties")
+            startActivity(intent)
+        }
 
     }
 }
